8c8cbfd297672b13e667170ffc4dfa22d287cdfd
StephNereah <stphnadhiambo@gmail.com>
2024-10-15 14:37:16 +0000

    Install Jest, Babel, and ESLint

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'jest',
+  ],
+  rules: {
+    'no-console': 'off',
+  },
+};
