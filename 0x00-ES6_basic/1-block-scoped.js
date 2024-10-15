caeb0f4d583ea5d5ee7c54d5e2ef37c6dc98b7a2
StephNereah <stphnadhiambo@gmail.com>
2024-10-15 17:27:19 +0000

    Clear eslint error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -9,3 +10,4 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
+/* eslint-enable */
